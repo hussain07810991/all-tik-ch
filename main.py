9f9a90c320eab81acf0159eabeb4d6d254ead03d
عفريتون <145981238+hussain07810991@users.noreply.github.com>
2024-06-10 16:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -193,6 +193,6 @@ async def say_hello4(name):
     except:
         return {"AFRIT":"𝐓𝐋𝐄:@AFR_0 | @LPB_B","Check":"False"}
 #Facebook
-#@app.get("/AFRIT/Facebook")
+@app.get("/AFRIT/Facebook")
 async def say_hello02():
     return {"AFRIT":"Tle:@AFR_0 | @LPB_B","FACEBook":"23934021671137812393402167113781","FACEBOok":"699a699f696feaf2699a699f696feaf2","FACEBOOK":"66b8f18ca748626866b8f18ca7486268","FACEBOOk":"73400e380482b97f73400e380482b97f",}
