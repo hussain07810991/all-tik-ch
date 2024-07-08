d588e4044e9066ea2a1392c8c089853c591406e3
عفريتون <145981238+hussain07810991@users.noreply.github.com>
2024-07-08 17:03:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -161,10 +161,10 @@ def check_email(email):
 
 
 #@app.get("/AFRIT/Checker-BOT-Twit={name}")
-@app.get("/AFRIT/Checker-BOT-Facebook={name}")
+#@app.get("/AFRIT/Checker-BOT-Facebook={name}")
 #Tiktok
 
-#@app.get("/AFRIT/Checker-BOT-Tiktok={name}")
+@app.get("/AFRIT/Checker-BOT-Tiktok={name}")
 #INSTA
 #@app.get("/AFRIT/Checker-BOT-INSTA={name}")
 #@app.get("/AFRIT/Checker-BOT-INSTA-User={name}")
@@ -196,7 +196,7 @@ async def say_hello4(name):
     except:
         return {"AFRIT":"𝐓𝐋𝐄:@AFR_0 | @LPB_B","Check":"False"}
 #Facebook
-@app.get("/AFRIT/Facebook")
+#@app.get("/AFRIT/Facebook")
 async def say_hello02():
     return {"AFRIT":"Tle:@AFR_0 | @LPB_B","FACEBook":"23934021671137812393402167113781","FACEBOok":"699a699f696feaf2699a699f696feaf2","FACEBOOK":"66b8f18ca748626866b8f18ca7486268","FACEBOOk":"73400e380482b97f73400e380482b97f",}
 
